7c91b6f596408a9e7eb17e2f00de274051afc892
Lucas Moreira <lucas@moreiradevelopment.io>
2019-06-27 13:50:47 -0400

    Added a little bit of documentation to clear things up

--- src/apex-data.js
+++ src/apex-data.js
@@ -1,11 +1,21 @@
-// APEX MEDIA SITE DATA
+// This file is intended to serve as a flat file DB to centralize
+// some of the repeated app data. This is not a substitute for a
+// Dynamic DB but can work well for non-dynamic features and for
+// brochure websites
+//
+// 1. Create variables to contain JSON objects with data.
+// 2. Export the variable so it can be imported by components.
+// 3. Import the JSON Objects that you need for the given component.
 
+// General App data
 let appData = {
   blackBrand: 'apex-logo-black.svg',
   whiteBrand: 'apex-logo-white.svg',
 };
 
+// Main Navigation Links
 let mainNavigation = {
+  // Refer to routes.js file for available routes.
   links: [
     {
       linkName: 'Home',
@@ -35,4 +45,8 @@ let mainNavigation = {
   ]
 };
 
-export { appData, mainNavigation };
+// Export named variables
+export {
+  appData,
+  mainNavigation
+};
