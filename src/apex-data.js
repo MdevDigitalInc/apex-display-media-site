e32c0f0b9fbe512b20a9c5d6cb607cc64bae39d3
Lucas Moreira <lucas@moreiradevelopment.io>
2019-06-28 13:55:44 -0400

    Added data structure & placeholder image

--- src/apex-data.js
+++ src/apex-data.js
@@ -45,8 +45,68 @@ let mainNavigation = {
   ]
 };
 
+// Home Page Template DATA
+let homePage = {
+  // Featured Services Component Data
+  // ARRAY - For looping through featured services.
+  featuredServices: [
+    {
+      heading: 'Outdoor LED Signage',
+      copy: 'Give your business the most effective platform to advertise, build brand awareness and deliver results. The high visibility of outdoor LEDs will give your business an edge over the competition.',
+      factInfo: 'A majority of consumers (<span class="u-bold">85</span>%) state that digital signage could influence future purchase decisions.',
+      // Button DATA
+      action: {
+        route: '#TODO',
+        a11y: 'LED Signage Benefits.',
+        text: 'Learn More'
+      },
+      // Image DATA
+      media: {
+        source: '/home/featured-service-placeholder.jpeg',
+        gif: false,
+        a11y: 'Placeholder - TODO update'
+      },
+    },
+    {
+      heading: 'Indoor Digital Signage',
+      copy: 'Help customers understand your business better. Express your brand messaging, promotions and expertly tell your story on our HD commerical indoor displays.',
+      factInfo: 'Digital signage will become your most valuable on-site marketing tool.',
+      // Button DATA
+      action: {
+        route: '#TODO',
+        a11y: 'Indoor Digital Signage Benefits.',
+        text: 'Learn More'
+      },
+      // Image DATA
+      media: {
+        source: '/home/featured-service-placeholder.jpeg',
+        gif: false,
+        a11y: 'Placeholder - TODO update'
+      },
+    },
+    {
+      heading: 'Digital Media Management',
+      copy: 'Quality design and motion graphics help convey eye-catching messaging.  With several membership packages available, there’s sure to be an option to fit your business needs.',
+      factInfo: 'Expertly designed content maximizes your ROI in hardware.',
+      // Button DATA
+      action: {
+        route: '#TODO',
+        a11y: 'How we keep your sign shining.',
+        text: 'Learn More'
+      },
+      // Image DATA
+      media: {
+        source: '/home/featured-service-placeholder.jpeg',
+        gif: false,
+        a11y: 'Placeholder - TODO update'
+      },
+    }
+  ]
+}
+
 // Export named variables
 export {
   appData,
-  mainNavigation
+  mainNavigation,
+  homePage
 };
