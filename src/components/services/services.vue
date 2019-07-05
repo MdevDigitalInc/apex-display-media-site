571d24269346b58e16e180b4265529b1d59bb126
Dorian <dorian@mdev.digital>
2019-07-05 16:16:11 -0400

    start setting up routes + component files

---
+++ src/components/services/services.vue
@@ -0,0 +1,40 @@
+
+<template lang="pug">
+  section.apex-main-content
+    h1
+      | Services
+</template>
+
+
+
+
+<script>
+
+// Import SEO From File
+//import { stagingBuild, template, social, general }       from '../../seo-meta.js';
+
+// Data Import
+//import { homePage } from '../../apex-data.js';
+
+export default {
+  name: 'ServicesPage',
+
+  data: function(){
+    return {
+    };
+  }
+};
+</script>
+
+
+
+<style lang="scss">
+
+/*-------------------------------------*/
+/* BASE TEMPLATE Component Styles
+/--------------------------------------*/
+
+
+/*--------------------------------------*/
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,10 @@ import AuthComponent from './components/auth/Authentication.vue';
 import LoginComponent from './components/auth/Login.vue';
 import RegisterComponent from './components/auth/Register.vue';
 import ResetComponent from './components/auth/Reset.vue';
-
+// Services Components
+import Services from './components/services/services.vue';
+import ServicesDigitalMediaManagement from './components/services/service--digital-media-management.vue';
+import ServicesIndoorDigital from './components/services/service--indoor-digital.vue';
 // Export routes as named constant array.
 // Routes consist of desired path + bound parent component
 export const routes = [
@@ -44,6 +47,21 @@ export const routes = [
       {
         path: "reset",
         component: ResetComponent
+      }
+    ]
+  },
+  {
+    path: '/services',
+    component: Services,
+    redirect: '/services/digital-media-management',
+    children: [
+      {
+        path: "digital-media-management",
+        component: ServicesDigitalMediaManagement ,
+      },
+      {
+        path: "indoor-digital",
+        component: ServicesIndoorDigital
       }
     ]
   }
