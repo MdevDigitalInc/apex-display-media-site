fd6fc572a2a2eb78908ce832852a862a9ca91496
Lucas Moreira <lucas@moreiradevelopment.io>
2019-07-10 16:36:06 -0400

    Finished Desktop and basic responsive

--- src/components/services/service--sample.vue
+++ src/components/services/service--sample.vue
@@ -32,20 +32,33 @@ export default {
   position: relative;
   padding-left: 12%;
 
+  @media #{$portrait} {
+    padding-left: 0;
+  }
 
   .apex-sample-media {
     width: 50%;
     background-size: cover;
     background-repeat: no-repeat;
     box-shadow: 0 0 30px rgba( 0, 0, 0, .1);
+
+    @media #{ $portrait } {
+      padding-top: 90%;
+      width: 100%;
+    }
   }
 
   .apex-sample-content {
     width: 50%;
     padding: 8%;
 
+    @media #{ $portrait } {
+      width: 100%;
+    }
+
     p {
       line-height: 1.2;
+      margin-bottom: 15px;
     }
   }
 
